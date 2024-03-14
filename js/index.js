b9ce57695fcabaa5d6dc267092184d2477ebb87a
Trần Việt Anh <95759256+tranvietanh20112001@users.noreply.github.com>
2024-03-14 21:51:30 +0700

    first

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  var image = document.getElementById("animatedImage");
+  var hello = document.getElementById("animatedHello");
+  var start = document.getElementById("start");
+  image.style.opacity = 1;
+  hello.style.opacity = 1;
+  start.style.opacity = 1;
+};
